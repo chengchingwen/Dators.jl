9a16d6fc2adf5b428b5452b2657902dcf73415af
chengchingwen <adgjl5645@hotmail.com>
2021-07-07 09:34:12 +0800

    default to single loop

--- src/executor.jl
+++ src/executor.jl
@@ -3,41 +3,35 @@ struct Executor{F, T}
   task::T
 end
 
+map_do_map_f(ch, f, con, do_post_f=do_post_f) = foreach(src->map_do_f(ch, f, src, do_post_f) ,  con)
+
 do_post_f(ch, result) = put!(ch, result)
 map_do_f(ch, f, src, do_post_f=do_post_f) = foreach(Base.Fix1(do_post_f, ch)∘f, src)
 
 function Executor(f, con, dst, mode::Type{Async}, id=0; map_do_f=map_do_f, do_post_f=do_post_f)
   task = @async begin
-    for src in con
-      map_do_f(dst, f, src, do_post_f)
-    end
+      map_do_f(dst, f, con, do_post_f)
   end
   return Executor(f, task)
 end
 
 function Executor(f, con, dst, mode::Type{Thread}, id=0; map_do_f=map_do_f, do_post_f=do_post_f)
   task = Threads.@spawn begin
-    for src in con
-      map_do_f(dst, f, src, do_post_f)
-    end
+      map_do_f(dst, f, con, do_post_f)
   end
   return Executor(f, task)
 end
 
 function Executor(f, con, dst, mode::Type{Process}, id=0; map_do_f=map_do_f, do_post_f=do_post_f)
   task = Distributed.@spawnat :any begin
-    for src in con
-      map_do_f(dst, f, src, do_post_f)
-    end
+      map_do_f(dst, f, con, do_post_f)
   end
   return Executor(f, task)
 end
 
 function Executor(f, con, dst, mode::Type{ProcessWithIDs}, id; map_do_f=map_do_f, do_post_f=do_post_f)
   task = Distributed.@spawnat id begin
-    for src in con
-      map_do_f(dst, f, src, do_post_f)
-    end
+      map_do_f(dst, f, con, do_post_f)
   end
   return Executor(f, task)
 end
