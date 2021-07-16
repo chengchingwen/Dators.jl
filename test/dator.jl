1be3980b3d47b48f3eacdad2d665caa90ae99115
JingYu Ning <foldfelis@gmail.com>
2021-07-16 08:10:08 +0800

    add test for future work

--- test/dator.jl
+++ test/dator.jl
@@ -18,7 +18,7 @@
     @test all(result)
 end
 
-@testset "multi thread" begin
+@testset "multi thread drop_last=false" begin
     src = Dators.CreateSrc(Dators.Thread(1)) do
         Channel() do ch
             for i = 1:100
@@ -41,3 +41,27 @@ end
 
     @test result == [x^2 for x in 1:100]
 end
+
+# @testset "multi thread drop_last=true" begin
+#     src = Dators.CreateSrc(Dators.Thread(1)) do
+#         Channel() do ch
+#             for i = 1:100
+#                 put!(ch, i)
+#             end
+#         end
+#     end
+
+#     dator = Dators.Dator(x->x*x, 1, src;  compute_type=Dators.Thread(4))
+#     bd = Dators.batch(dator, 5, true)
+#     Dators.start!(bd)
+
+#     result = Int64[]
+#     for (i, d) in enumerate(bd)
+#         for data in d[1]
+#             push!(result, data)
+#         end
+#     end
+#     sort!(result)
+
+#     @test result == [x^2 for x in 1:100]
+# end
